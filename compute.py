d0806d68e0fa09031c690736c2d7388f2392135b
jdkato <joseph@jdkato.io>
2022-07-26 14:58:58 -0700

    Update

--- compute.py
+++ compute.py
@@ -1,5 +1,7 @@
 import pathlib
+import datetime
 
+import pandas
 import pandas as pd
 import streamlit as st
 
@@ -8,6 +10,112 @@ from sqlalchemy import create_engine
 
 
 DATA = pathlib.Path("csv")
+
+
+def players(season=1):
+    teams = pd.read_csv(DATA / "s1" / "teams.csv")
+
+    made = []
+    for _, entry in teams.iterrows():
+        team = entry["Team"]
+
+        players = []
+        games = []
+        for filename in (DATA / f"s{season}" / "boxscores" / f"{team}").glob("*.csv"):
+            gdf = pd.read_csv(filename)
+            games.append(gdf)
+            players.extend(gdf["Player"])
+
+        totals = pd.concat(games, axis=0, ignore_index=True)
+        for player in set(players):
+            games_played = players.count(player)
+
+            fgm = float(totals.loc[totals["Player"] == player, "FGM"].sum())
+            fga = float(totals.loc[totals["Player"] == player, "FGA"].sum())
+
+            tpm = float(totals.loc[totals["Player"] == player, "3PM"].sum())
+            tpa = float(totals.loc[totals["Player"] == player, "3PA"].sum())
+
+            made.append(
+                {
+                    "Player": player,
+                    "Team": team,
+                    "GP": games_played,
+                    "FGM": fgm,
+                    "FGA": fga,
+                    "FG%": fgm / fga if fga else 0,
+                    "3PM": tpm,
+                    "3PA": tpa,
+                    "3P%": tpm / tpa if tpa else 0,
+                    "TRB": totals.loc[totals["Player"] == player, "REB"].sum()
+                    / games_played,
+                    "AST": totals.loc[totals["Player"] == player, "AST"].sum()
+                    / games_played,
+                    "STL": totals.loc[totals["Player"] == player, "STL"].sum()
+                    / games_played,
+                    "BLK": totals.loc[totals["Player"] == player, "BLK"].sum()
+                    / games_played,
+                    "TOV": totals.loc[totals["Player"] == player, "TO"].sum()
+                    / games_played,
+                    "PTS": totals.loc[totals["Player"] == player, "PTS"].sum()
+                    / games_played,
+                },
+            )
+
+    return pd.DataFrame.from_records(
+        made,
+        columns=[
+            "Player",
+            "Team",
+            "GP",
+            "FGM",
+            "FGA",
+            "FG%",
+            "3PM",
+            "3PA",
+            "3P%",
+            "TRB",
+            "AST",
+            "STL",
+            "BLK",
+            "TOV",
+            "PTS",
+        ],
+    )
+
+
+def recent_games():
+    games = DATA / "s1" / "games"
+
+    made = []
+    for game in games.glob("*.csv"):
+        away, home = [g.split(".csv")[0] for g in game.name.split("-")[:2]]
+
+        df = pandas.read_csv(game)
+        m_timestamp = game.stat().st_mtime
+        m_time = datetime.datetime.fromtimestamp(m_timestamp)
+        m_day = m_time.strftime("%m/%d/%Y")
+
+        made.append({
+            "Home": home,
+            "Away": away,
+            "HScore": df.iloc[1, 5],
+            "AScore": df.iloc[0, 5],
+            "Date": m_day
+        })
+
+    df = pd.DataFrame.from_records(
+        made,
+        columns=[
+            "Home",
+            "Away",
+            "HScore",
+            "AScore",
+            "Date"
+        ],
+    )
+
+    return df
 
 
 def leaders(stat, show, stats_df):
@@ -251,7 +359,7 @@ def compute_record(team):
 
     games = (DATA / "s1" / "games").glob("**/*")
     for game in games:
-        teams = [t.split(".csv")[0] for t in game.name.split("-")]
+        teams = [t.split(".csv")[0] for t in game.name.split("-")[:2]]
         if team in teams:
             idx = teams.index(team)
             rdf = pd.read_csv(game.absolute())
@@ -311,43 +419,41 @@ if __name__ == "__main__":
     stats_df = summary(season=1)
 
     standings_df = compute_records()
-    standings_df.to_sql(name="Standings", con=engine)
+    standings_df.to_sql(name="Standings", con=engine, if_exists='replace')
     # standings_df.to_csv("build/standings.csv", sep=',', index=False)
 
     pts_df = leaders("PTS", ["Player", "GP", "PTS"], stats_df)
-    pts_df.to_sql(name="PTS", con=engine)
+    pts_df.to_sql(name="PTS", con=engine, if_exists='replace')
 
     fgp_df = leaders("FG%", ["Player", "GP", "FG%", "FGM"], stats_df)
-    fgp_df.to_sql(name="FGP", con=engine)
+    fgp_df.to_sql(name="FGP", con=engine, if_exists='replace')
 
     ast_df = leaders("AST", ["Player", "GP", "AST"], stats_df)
-    ast_df.to_sql(name="AST", con=engine)
+    ast_df.to_sql(name="AST", con=engine, if_exists='replace')
 
     tmp_df = leaders("3PG", ["Player", "GP", "3PG"], stats_df)
-    tmp_df.to_sql(name="TPG", con=engine)
+    tmp_df.to_sql(name="TPG", con=engine, if_exists='replace')
 
     tpp_df = leaders("3P%", ["Player", "GP", "3P%", "3PM"], stats_df)
-    tpp_df.to_sql(name="TPP", con=engine)
+    tpp_df.to_sql(name="TPP", con=engine, if_exists='replace')
 
     reb_df = leaders("TRB", ["Player", "GP", "TRB"], stats_df)
-    reb_df.to_sql(name="REB", con=engine)
+    reb_df.to_sql(name="REB", con=engine, if_exists='replace')
 
     blk_df = leaders("BLK", ["Player", "GP", "BLK"], stats_df)
-    blk_df.to_sql(name="BLK", con=engine)
+    blk_df.to_sql(name="BLK", con=engine, if_exists='replace')
 
     stl_df = leaders("STL", ["Player", "GP", "STL"], stats_df)
-    stl_df.to_sql(name="STL", con=engine)
+    stl_df.to_sql(name="STL", con=engine, if_exists='replace')
 
     high_df = highs()
-    high_df.to_sql(name="Highs", con=engine)
+    high_df.to_sql(name="Highs", con=engine, if_exists='replace')
 
     total_df = totals()
-    total_df.to_sql(name="Team", con=engine)
-    #total_df.to_csv("build/total.csv", sep=',', index=False)
+    total_df.to_sql(name="Team", con=engine, if_exists='replace')
 
     opp_df = op_totals()
-    opp_df.to_sql(name="Opponent", con=engine)
-    #opp_df.to_csv("build/opp.csv", sep=',', index=False)
+    opp_df.to_sql(name="Opponent", con=engine, if_exists='replace')
 
     rows = []
     for i, row in total_df.iterrows():
@@ -361,6 +467,11 @@ if __name__ == "__main__":
     diff_df = pd.DataFrame(rows)
     diff_df = diff_df.sort_values(by=["PTS"], ascending=False)
     diff_df = diff_df.reset_index(drop=True)
-    diff_df.to_sql(name="Differential", con=engine)
-    #diff_df.to_csv("build/diff.csv", sep=',', index=False)
+    diff_df.to_sql(name="Differential", con=engine, if_exists='replace')
+
+    games_df = recent_games()
+    games_df.to_sql(name="Games", con=engine, if_exists='replace')
+
+    players_df = players()
+    players_df.to_sql(name="Players", con=engine, if_exists='replace')
 
